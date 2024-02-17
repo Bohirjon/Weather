24456f1dd4518c2d4d0b3f6c8ebe2df67125f1ea
Bohirjon Akhmedov <bohirjon.a@gmail.com>
2024-02-17 15:30:30 +0500

    Initial Commit

---
+++ Weather/WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApp.swift
+//  Weather
+//
+//  Created by Bohirjon Akhmedov on 2/17/24.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
